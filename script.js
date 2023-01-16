475cfcb7c75f52590d8fc7c99db9bafa93f95708
Kopalle Manoj <103321894+kopallemanoj@users.noreply.github.com>
2023-01-16 15:08:42 +0530

    Student Enrollment

--- script.js
+++ script.js
@@ -1,106 +1,172 @@
-const taskInput = document.querySelector(".task-input input"),
-    filters = document.querySelectorAll(".filters span"),
-    clearAll = document.querySelector(".clear-btn"),
-    taskBox = document.querySelector(".task-box");
+var form = document.querySelector("#userForm");
+const allUsersData = [];
 
-let editId,
-    isEditTask = false,
-    todos = JSON.parse(localStorage.getItem("todo-list"));
+// ------------------function to reset the form------------------
+const resetForm = function() {
+    form.classList.remove('was-validated')
+    const name = document.getElementById('name');
+    name.value = "";
 
-filters.forEach(btn => {
-    btn.addEventListener("click", () => {
-        document.querySelector("span.active").classList.remove("active");
-        btn.classList.add("active");
-        showTodo(btn.id);
-    });
-});
+    const email = document.getElementById('email');
+    email.value = "";
 
-function showTodo(filter) {
-    let liTag = "";
-    if (todos) {
-        todos.forEach((todo, id) => {
-            let completed = todo.status == "completed" ? "checked" : "";
-            if (filter == todo.status || filter == "all") {
-                liTag += `<li class="task">
-                            <label for="${id}">
-                                <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
-                                <p class="${completed}">${todo.name}</p>
-                            </label>
-                            <div class="settings">
-                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
-                                <ul class="task-menu">
-                                    <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
-                                    <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
-                                </ul>
-                            </div>
-                        </li>`;
-            }
-        });
+    const website = document.getElementById('website');
+    website.value = "";
+
+    const image = document.getElementById('image');
+    image.value = "";
+
+    const genderEl = document.querySelectorAll('input[name="gender"]');
+    for (const rb of genderEl) {
+        rb.checked = false;
     }
-    taskBox.innerHTML = liTag || `<span>You don't have any task here</span>`;
-    let checkTask = taskBox.querySelectorAll(".task");
-    !checkTask.length ? clearAll.classList.remove("active") : clearAll.classList.add("active");
-    taskBox.offsetHeight >= 300 ? taskBox.classList.add("overflow") : taskBox.classList.remove("overflow");
-}
-showTodo("all");
 
-function showMenu(selectedTask) {
-    let menuDiv = selectedTask.parentElement.lastElementChild;
-    menuDiv.classList.add("show");
-    document.addEventListener("click", e => {
-        if (e.target.tagName != "I" || e.target != selectedTask) {
-            menuDiv.classList.remove("show");
+    const skillEl = document.querySelectorAll('input[name="skill"]');
+    for (const rb of skillEl) {
+        rb.checked = false;
+    }
+};
+
+// --------------------function to get the data of the form----------------------
+
+const getData = function() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const website = document.getElementById('website').value;
+    const image = document.getElementById('image').value;
+    let gender;
+    let skills = [];
+
+    const genderEl = document.querySelectorAll('input[name="gender"]');
+    for (const rb of genderEl) {
+        if (rb.checked) {
+            gender = rb.value;
+            break;
         }
-    });
-}
+    };
 
-function updateStatus(selectedTask) {
-    let taskName = selectedTask.parentElement.lastElementChild;
-    if (selectedTask.checked) {
-        taskName.classList.add("checked");
-        todos[selectedTask.id].status = "completed";
+    const skillEl = document.querySelectorAll('input[name="skill"]');
+    for (const rb of skillEl) {
+        if (rb.checked) {
+            skills.push(rb.value);
+        }
+    }
+    return { name, email, website, image, gender, skills };
+};
+
+//-----------------------adding event listner to the "enroll student" button with type submit to submit the form
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    if (form.checkValidity()) {
+        const data = getData();
+        allUsersData.push(data);
+        printResult(data);
+        resetForm();
+
+
     } else {
-        taskName.classList.remove("checked");
-        todos[selectedTask.id].status = "pending";
-    }
-    localStorage.setItem("todo-list", JSON.stringify(todos))
-}
-
-function editTask(taskId, textName) {
-    editId = taskId;
-    isEditTask = true;
-    taskInput.value = textName;
-    taskInput.focus();
-    taskInput.classList.add("active");
-}
-
-function deleteTask(deleteId, filter) {
-    isEditTask = false;
-    todos.splice(deleteId, 1);
-    localStorage.setItem("todo-list", JSON.stringify(todos));
-    showTodo(filter);
-}
-
-clearAll.addEventListener("click", () => {
-    isEditTask = false;
-    todos.splice(0, todos.length);
-    localStorage.setItem("todo-list", JSON.stringify(todos));
-    showTodo()
+        form.classList.add('was-validated');
+    };
+    removeSpan();
 });
 
-taskInput.addEventListener("keyup", e => {
-    let userTask = taskInput.value.trim();
-    if (e.key == "Enter" && userTask) {
-        if (!isEditTask) {
-            todos = !todos ? [] : todos;
-            let taskInfo = { name: userTask, status: "pending" };
-            todos.push(taskInfo);
-        } else {
-            isEditTask = false;
-            todos[editId].name = userTask;
+// --------------function to remove the span tag ("fill the form to enroll the students")
+
+function removeSpan() {
+    var span = document.getElementById("span");
+    if (span) {
+        span.remove();
+    }
+
+};
+
+// ------------------function to print the form data in the right side of div by genrating html elments inside the div.
+
+function printResult(data) {
+    const resultEl = document.getElementById('enrolled-students');
+    let sectionHeading = null;
+    if (allUsersData.length == 1) {
+
+        sectionHeading = document.createElement('div');
+        const description = document.createElement('p');
+        description.innerHTML = "Description";
+        description.className = "description";
+
+        const image = document.createElement('p');
+        image.innerHTML = "Image"
+        image.className = "Image";
+
+        sectionHeading.className = "sectionHeading";
+        sectionHeading.append(description, image);
+    };
+
+    const wrapper = document.createElement('div');
+    wrapper.className = "wrapper";
+    wrapper.addEventListener('click', function(e) {
+        console.log(e.target.className);
+        if (e.target.className.includes('userDeleteBtn')) {
+            console.log('aaadfasdfasdf');
+            e.currentTarget.remove();
         }
-        taskInput.value = "";
-        localStorage.setItem("todo-list", JSON.stringify(todos));
-        showTodo(document.querySelector("span.active").id);
-    }
-});
+
+    });
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML = "+";
+    deleteBtn.className = "userDeleteBtn";
+
+    const textInfoContainer = document.createElement('div');
+    textInfoContainer.className = "textInfoContainer";
+
+    const imageContainer = document.createElement('div');
+    imageContainer.className = "imageContainer";
+
+    const imageHyperlink = document.createElement('a');
+    imageHyperlink.href = data.image;
+    imageHyperlink.target = "_blank";
+
+
+    let name = document.createElement('p');
+    name.className = "infoText userName";
+    name.innerHTML = data.name;
+
+    let gender = document.createElement('p');
+    gender.className = "infoText gender";
+    gender.innerHTML = data.gender;
+
+    let email = document.createElement('p');
+    email.className = "infoText email";
+    email.innerHTML = data.email;
+
+    let website = document.createElement('a');
+    website.className = "infoText website";
+    website.innerHTML = data.website;
+    website.href = data.website;
+    website.target = "_blank";
+
+    let skills = document.createElement('p');
+    skills.className = "infoText skills";
+    skills.innerHTML = data.skills.join(', ');
+
+
+    let userImage = document.createElement('img');
+    userImage.className = "userImage";
+    userImage.src = data.image;
+
+
+    textInfoContainer.append(name, gender, email, website, skills);
+    imageHyperlink.appendChild(userImage);
+    imageContainer.appendChild(imageHyperlink);
+
+    wrapper.append(textInfoContainer, imageContainer, deleteBtn);
+
+    if (sectionHeading == null) {
+        resultEl.append(wrapper);
+    } else {
+        resultEl.append(sectionHeading, wrapper)
+    };
+
+};
+
+// The end------------------
